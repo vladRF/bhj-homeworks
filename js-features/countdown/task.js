f6c7d19680b2ee9057b147f3228b69a560757c87
Vladislav Bologov <wlad.bologov@gmail.com>
2025-07-10 16:54:26 +0300

    исправление первой задачи

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+const Timer = function () {
+    const output = document.getElementById("timer")
+    if (output.textContent != 0) {
+        output.textContent -= 1;
+    }
+
+    if (output.textContent == 0) {
+        alert("Вы победили в конкурсе!")
+        clearInterval(TimerID);
+    }
+
+}
+
+const TimerID = setInterval(Timer, 500);
